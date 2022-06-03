39f11edbd8d718d9bb8b7430fc9af44476ef4b62
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-06-02 23:16:39 -0500

    Paso 6: Creando nueva card con dependencia de trello

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
     throw new Error('No hay configuración con Api key y Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "62993927622b613b8a92d14b", 
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	}
+);
+
+
